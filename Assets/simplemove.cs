a38fd9495cff0fca92f889fe7bcd7b95d0f10843
Unknown <jrodolfo11maci@gmail.com>
2018-01-27 20:10:06 -0800

    TEST movement with blur
    this is test mode not to consider final move mechanic
    it shows the form of the blur on movement

---
+++ Assets/simplemove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class simplemove : MonoBehaviour {
+    public GameObject target;
+	// Use this for initialization
+	void Start () {
+        transform.LookAt(target.transform);
+        transform.Rotate(Vector3.right * 180);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0,0,-0.01f),Space.Self);
+	}
+}
