3a736737573aa5cffde6090778828b25655862f0
Carolyn Nicita <cnicita.vr@gmail.com>
2018-01-28 13:54:16 -0800

    Revert "testlion interaction with vr controller"
    This reverts commit ba7cbb55cc58a3cc0d405518a0d9ee36bed364c7.

--- Assets/simplemove.cs
+++ Assets/simplemove.cs
@@ -6,17 +6,12 @@ public class simplemove : MonoBehaviour {
     public GameObject target;
 	// Use this for initialization
 	void Start () {
-        if (!target)
-        {
-            target = GameObject.Find("Camera (eye)");
-        }
         transform.LookAt(target.transform);
         transform.Rotate(Vector3.right * 180);
     }
 	
 	// Update is called once per frame
 	void Update () {
-       
         transform.Translate(new Vector3(0,0,-0.01f),Space.Self);
 	}
 }
